ccdb0e8b876b5f8343b73d12c430bf47061058d5
dxb2503223134 <2503223134@qq.com>
2020-05-25 19:57:50 +0800

    添加项目文件。

---
+++ TestManagement/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestManagement.Models;
+
+namespace TestManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        public string Index()
+        {
+            //返回学生名字
+            return _studentRepository.GetStudent(1).Name;
+        }
+    }
+}
---
+++ TestManagement/Models/MockStudentRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestManagement.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        private List<Student> _students;
+        public MockStudentRepository()
+        {
+            _students = new List<Student>()
+            {
+                new Student{ Id=1,Name="张三",ClassName="一年级",Email="2503223134@qq.com"},
+                new Student{ Id=2,Name="李四",ClassName="二年级",Email="13054119237@qq.com"},
+                new Student{ Id=3,Name="王五",ClassName="三年级",Email="15096339906@qq.com"}
+            };
+        }
+        public Student GetStudent(int id)
+        {
+            return _students.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
