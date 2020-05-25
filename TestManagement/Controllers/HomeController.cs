db3ce39074290f306fbe9d634718edf10d517d7b
dxb2503223134 <2503223134@qq.com>
2020-05-25 23:00:26 +0800

    0525

--- TestManagement/Controllers/HomeController.cs
+++ TestManagement/Controllers/HomeController.cs
@@ -10,14 +10,22 @@ namespace TestManagement.Controllers
     public class HomeController : Controller
     {
         private readonly IStudentRepository _studentRepository;
+
         public HomeController(IStudentRepository studentRepository)
         {
             _studentRepository = studentRepository;
         }
+
         public string Index()
         {
-            //返回学生名字
+            // 返回学生名字
             return _studentRepository.GetStudent(1).Name;
+        }
+        public IActionResult Details()
+        {
+            var model = _studentRepository.GetStudent(1);
+            //return View("~/MyViews/Test.cshtml");
+            return View("../../MyViews/Details"); //去掉后缀
         }
     }
 }
--- TestManagement/Startup.cs
+++ TestManagement/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using TestManagement.Models;
 
 namespace TestManagement
 {
@@ -15,7 +16,10 @@ namespace TestManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().AddXmlSerializerFormatters();
+
+            // 依赖注入服务
+            services.AddSingleton<IStudentRepository, MockStudentRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
