0687703518c8391a0ae66d5703ccaa8c8d4c7a71
dxb2503223134 <2503223134@qq.com>
2020-06-26 15:08:31 +0800

    0626

---
+++ 异步编程/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace 异步编程
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run(() =>
+              {
+                  Console.WriteLine("Foo");
+                  return 3;
+              });
+
+            var result = task.Result;
+
+            Console.WriteLine(result);
+
+            //var res = await GetData();
+
+            //Console.WriteLine(res);
+        }
+
+        public static async Task<string> GetData()
+        {
+            await Task.Delay(1);
+            return "Hello";
+        }
+    }
+}
